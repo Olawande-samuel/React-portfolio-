ff69e70613523bf5d3ec7623965d0662415ac739
samueldacolie <samueldacoal@gmail.com>
2021-02-28 12:11:33 +0100

    new commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -21,29 +21,30 @@ function About() {
 						</div>
 					</div>
 					<div className="right col-md-6">
-						<h2 >About me</h2>
-						
+						<h2>About me</h2>
+
 						<p>
-							<span>Hi,</span> I'm sure you must have been wondering 'who is that guy over
-							there?'. Well, that's me, Olawande, a frontend developer from
-							Nigeria. I build user friendly, aesthetically pleasing, modern and
-							responsive websites and web applications. I'm graduate of English
-							and I am gradually working my way up the MERN stack in a bid to
-							becoming a fullstack developer.
+							<span>Hi,</span> I'm sure you must have been wondering, 'who is
+							that guy over there?' Well, that's me, Olawande, a web developer
+							from Nigeria. I build user-friendly, beautiful, modern and
+							responsive websites and web applications. I'm a graduate of
+							English, and I have chosen to gradually work my way up the MERN
+							stack. The goal is to become a full-stack developer.
 						</p>
 
-						<p> I love building web projects and
-							learning and applying new technologies as well as participanting
-							in activities that help me grow as a developer. I also enjoy
-							working together with people on projects. 
+						<p>
+							{" "}
+							I love building web projects, learning and applying new
+							technologies, and participating in activities that help me grow as
+							a developer. I also enjoy working together with people on
+							projects.
 						</p>
 						<p>
-							In my spare time, I
-							sometimes delve into the imaginary world of books where I go on
-							adventures with the characters. Other times, I rewatch my
-							favourite tvshows- Futurama and Bojack Horseman
+							In my spare time, I sometimes dive into the imaginary world of
+							books where I go on adventures with the characters. Other times, I
+							rewatch my favourite tv shows- Futurama and Bojack Horseman.
 						</p>
-						<p> 
+						<p>
 							Currently seeking gigs or junior developer positions. <br />
 							Here are a few technologies I've been working with recently:
 						</p>
